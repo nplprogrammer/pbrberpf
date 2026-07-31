89a4aa33b79b532539ebd736112ca52ef91a2252
NPL Programmer <nplprogrammer@gmail.com>
2026-07-31 16:19:34 -0600

    save plugin settings

--- includes/Admin.php
+++ includes/Admin.php
@@ -16,24 +16,10 @@ class Admin {
 	<form method="post" action="options.php">
 <?PHP
 			settings_fields('pbrberpf');
-			do_settings_sections('pbrberpf');
-		//	do_settings_sections('pbrberpf_settings');
-		//	do_settings_fields('pbrberpf', 'pbrberpf_settings');
+			do_settings_sections('pbrberpf_settings');
+			do_settings_fields('pbrberpf', 'pbrberpf_settings');
 ?>
 <br /><br />
-	Booking engine URL: <input type="text" name="srcurl" class="regular-text" 
-	size="100" value="<?PHP 
-//	echo esc_attr($option['beurl']);
-?>"/><br /><br />
-	Page 1 exclusions: <input type="text" name="sexa" class="regular-text"
-	size="100" value="<?PHP
-	//echo esc_attr($option['page1ex']);
-	?>" /><br /><br />
-	Page 2 exclusions: <input type="text" name="sexb" class="regular-text"
-	size="100" value="<?PHP
-	//echo esc_attr($option['page2ex']);
-	?>" /><br />
-<br />
 <div id="exsynexplain">
 Exclusion should be written as
 <b>Title 1#Title 2#Title 3..,</b>
@@ -61,36 +47,68 @@ Exclusion should be written as
 	static public function adm_setsec() {
 		echo "In adm_setsec";
 	}
-	static public function pbrberpf_field_pill_cb($args) {
-		$options = get_option('pbrberpf_opt');
-		var_dump($args);
-		var_dump($options);
+
+	static public function pbrberpf_field_hotelid_cb($args) {
+		$hotelid = get_option('pbrberpf_hotelid', '');
 ?>
-    Hotel ID: <input type="text" name="hotelid" size="10" class="regular-text" value="<?PHP
-	echo esc_attr($options['hotelid']);
-?>" /><br />	
+    Hotel ID: <input type="text" name="pbrberpf_hotelid" size="10" class="regular-text" value="<?PHP echo esc_attr($hotelid); ?>" /><br />
 <?PHP
 	}
+
+	static public function pbrberpf_field_beurl_cb($args) {
+		$beurl = get_option('pbrberpf_beurl', '');
+?>
+		Booking engine URL: <input type="text" name="pbrberpf_beurl" size="100"
+			class="regular-text" value="<?PHP echo esc_attr($beurl); ?>" /><br />
+<?PHP
+	}
+
+	static public function pbrberpf_field_pageaex_cb($args) {
+			$pageaex = get_option('pbrberpf_page1ex', '');
+?>
+			Page 1 exclusions: <input type="text" name="pbrberpf_page1ex" size="100" class="regular-text" value="<?PHP echo esc_attr($pageaex); ?>" /> (See below for syntax.)<br />
+<?PHP
+	}
+
+	static public function pbrberpf_field_pagebex_cb($args) {
+			$pagebex = get_option('pbrberpf_page2ex', '');
+?>
+			Page 2 exclusions: <input type="text" name="pbrberpf_page2ex" size="100" class="regular-text" value="<?PHP echo esc_attr($pagebex); ?>" /> (See below for syntax.)<br />
+<?PHP
+	}
+
+	static public function pbrberpf_field_cachetime_cb($args) {
+			$cachetime = get_option('pbrberpf_cachetime', 1);
+?>
+			Cache time: <input type="text" name="pbrberpf_cachetime" size="100" class="regular-text" value="<?PHP echo esc_attr($cachetime); ?>" /><br />
+<span id="pbrberpf_cache_explain">Set how many hours plugin should keep answers from booking engine cached. (Used in order to reduce possible calls to Booking Engine.)
+<br />
+<?PHP
+	}
+
 	static public function rsettings() {
-		register_setting('pbrberpf', 'pbrberpf_options');
-		add_settings_section('pbrberpf_settings',
-			'', 
-			__NAMESPACE__ . '\Admin::adm_setsec',
-			'pbrberpf');
-		add_settings_field('pbrberpf_opt',
-			__('Option', 'pbrberpf'),
-			__NAMESPACE__ . '\Admin::pbrberpf_field_pill_cb',
-			'pbrberpf', 'pbrberpf_settings',
-		);
+		register_setting('pbrberpf', 'pbrberpf_hotelid');
+		register_setting('pbrberpf', 'pbrberpf_beurl');
+		register_setting('pbrberpf', 'pbrberpf_page1ex');
+		register_setting('pbrberpf', 'pbrberpf_page2ex');
+		register_setting('pbrberpf', 'pbrberpf_cachetime');
+
+		add_settings_section('pbrberpf_settings', '', 
+			__NAMESPACE__ . '\Admin::adm_setsec', 'pbrberpf');
+
+		add_settings_field('pbrberpf_hotelid', __('Hotel ID', 'pbrberpf'), 
+			__NAMESPACE__ . '\Admin::pbrberpf_field_hotelid_cb', 
+			'pbrberpf', 'pbrberpf_settings',);
+		add_settings_field('pbrberpf_beurl', __('Booking Engine URL', 
+			'pbrberpf'), __NAMESPACE__ . '\Admin::pbrberpf_field_beurl_cb', 
+			'pbrberpf', 'pbrberpf_settings',);
+		add_settings_field('pbrberpf_page1ex', __('Page 1 exclusions', 
+			'pbrberpf'), __NAMESPACE__ . '\Admin::pbrberpf_field_pageaex_cb', 
+			'pbrberpf', 'pbrberpf_settings',);
+		add_settings_field('pbrberpf_page2ex', __('Page 2 exclusions', 
+			'pbrberpf'), __NAMESPACE__ . '\Admin::pbrberpf_field_pagebex_cb', 
+			'pbrberpf', 'pbrberpf_settings',);
+		add_settings_field('pbrberpf_cachetime', __('Cache Duration (hours)', 
+			'pbrberpf'), __NAMESPACE__ . '\Admin::pbrberpf_field_cachetime_cb', 			'pbrberpf', 'pbrberpf_settings',);
 	}
-/*
-		register_setting('pbrberpf_options',
-				'pbrberpf_opt',
-			[
-				'type' => 'array',
-				'default' => ''
-			]
-		);
-	}
-*/
 }
